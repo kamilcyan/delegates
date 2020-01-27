6e5ba722a13b4f885eb9bebfca7dcb6474942192
kamilcyan <kamil.lucjanek@wp.pl>
2020-01-27 20:15:07 +0100

    sas

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Hello World!+ {20:C2}");
+
+            Console.ReadKey();
+
+        }
+    }
+}
